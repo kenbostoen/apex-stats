31505b8b0a97e47b8213c9064a467448a739e823
ken <ken.bostoen@tothepoint.company>
2019-08-14 11:21:20 +0200

    initial commit

---
+++ backend/src/test/java/com/ken/apexstats/ApexstatsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ken.apexstats;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApexstatsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
